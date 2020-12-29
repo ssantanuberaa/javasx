1ca551e57d178a9d2b562c241ad634d7921c6cd9
Santanu Bera <ssantanuberaa@gmail.com>
2020-12-29 15:30:56 +0530

    x - 2.0.0

--- x.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-	env : "development",
-	debug : true,
-	theme : "vidhikarya",
-	basePath : "",
-	version : 97,
-	log : true,
-	target : "electron",
-};
--- x.js
+++ x.js
@@ -1,148 +1,93 @@
-import dom from "./lib/xDOM.js";
-import listener from "./lib/xListener.js";
-function X(){
-	this.dom = dom;
-	this.allComponentPromises = [];
+function x(options){
+	function init(props, options){
+		let component = {};
 
-	// Private Methods --
-	let extractNode = function(element){
-		let allNodes = [];
-		if (element.getAttribute("type") == null) {
-		}else if (element.getAttribute("type") == "xApp") {
-			return allNodes;
-		}else if (element.getAttribute("type").charAt(0) == "x") {
-			allNodes.push(element);
-		}
-		Array.prototype.forEach.call(element.children, (item)=>{
-			allNodes = allNodes.concat(extractNode(item));
-		});
-		return allNodes;
-	};
-	this.constructGlobalRef = function(){
-		// Create ref --
-		if (window.x == undefined){
-			window.x = {};
-		} 
-		if(window.x.apps == undefined){
-			window.x.apps = {};
-		}
-		if (window.x.store == undefined){
-			window.x.store = {};
-		}
-		if (window.x.listener == undefined) {
-			window.x.listener = listener;
-		}
-	};
-	// Instance Variables --
-	this.init = function(element, components, styles){
-
-		this.constructGlobalRef();
-
-		// Generating a Random key
-		let key = element.getAttribute("name");
-		if (key == null || key == undefined || key == "") {
-			key = new Date().valueOf();
+		// CSS --
+		if(options.css !== undefined){
+			loadCSS(options.css);
 		}
 
-		return new Promise(function(resolve, reject){
-			// window[x][key]
-			let s = window.x.apps[key] = {};
+		// Binding Props --
+		component.props = props;
 
-			if (element == null || element == undefined) {
-				console.log("X : Node is not found !");
-				resolve(s);
-				return;
-			}else{
-				// Load the element --
-			}
-			// Extracting all nodes whose type starts with "x" --
-			let allNodes = [];
-			Array.prototype.forEach.call(element.children, (item)=>{
-				allNodes = allNodes.concat(extractNode(item));
+		// Setting name --
+		component.name = options.name;
+
+		// Binding Data --
+		if(options.data !== undefined){
+			Object.keys(options.data).forEach(function(dataName){
+				component[dataName] = options.data[dataName];
 			});
-
-			
-			if (allNodes.length == 0) {
-				resolve(s);
-			}
-			allNodes.forEach((node, index)=>{
-				// Getting Information from the xElement --
-				let type = node.getAttribute("type");
-				let name = node.getAttribute("name");
-				let theme = node.getAttribute("theme");
-				if (name == null || name == undefined) {
-					name = new Date().valueOf();
-				}
-
-				// Creating xComponent and Storing the reference within s --
-				if (components[type] == undefined) {
-					console.log("Import : " + type);
-				}else{
-					this.allComponentPromises.push(this.buildModule(components[type], node, s, name));
-				}
-
-				// Loading Style --
-				if(styles == undefined || styles == null){
-					dom.loadStyle(node);	
-				}else{
-					dom.applyTheme(node);
-				}
-			});
-			Promise.all(this.allComponentPromises).then(function(){
-				if(styles != undefined){
-					this.bundleCss(key, styles);
-				}
-				resolve(s);
-			}.bind(this));
-
-		}.bind(this));
-	};
-	this.bundleCss = function(appName, styles){
-		let styleString = "";
-		Object.keys(styles).forEach(function(style, index){
-			styleString = styleString + styles[style];
-		});
-		let styleEl = document.createElement("style");
-		styleEl.setAttribute("type", "text/css");
-		styleEl.setAttribute("id", appName);
-		styleEl.textContent = styleString;
-		document.head.appendChild(styleEl);
-	};
-	this.buildModule = function(module, node, s, name){
-		return new Promise(function(resolve, reject){
-			try{
-				s[name] = new module(node);
-			}catch(error){
-				console.log("Error:");
-				console.log(node);
-				console.log(error);
-			}
-			resolve();
-		});
-	};
-	this.processing = function(element){
-		if (element == undefined) {
-			document.documentElement.style.opacity = "0";
-			document.documentElement.style.visiblity = "hidden";
-			document.documentElement.style.display = "none";	
-		}else{
-			element.style.opacity = "0";
-			element.style.visiblity = "hidden";
-			element.style.display = "none";
-		}		
-	};
-	this.processed = function(element){
-		if (element == undefined) {
-			document.documentElement.style.opacity = "1";
-			document.documentElement.style.visiblity = "visible";
-			document.documentElement.style.display = "block";
-		}else{
-			element.style.opacity = "1";
-			element.style.visiblity = "visible";
-			element.style.display = "block";
 		}
-		console.log("Processed");
+
+		// Binding Methods --
+		if(options.methods !== undefined){
+			Object.keys(options.methods).forEach(function(methodName){
+				component[methodName] = options.methods[methodName];
+			});
+		}
+
+		// Binding Scope --
+		component.$scope = [];
+		if(options.scope !== undefined){
+			createScope(options.scope, component);
+		}
+
+		// Render --
+		if(options.render != undefined){
+			component.render = options.render;
+			component.element = component.render();
+		}
+
+		// Apply root style --
+		if(props.style != undefined){
+			applyRootStyle(props, component);
+		}
+
+		// Mounted --
+		if(options.mounted != undefined){
+			component.mounted = options.mounted;
+			component.mounted();
+		}
+
+		// Check Option ClassNames --
+		if(props.classNames != undefined){
+			applyClassName(props, component);	
+		}
+
+		uniqueComponents(options);
+
+		return component;
 	};
+	function loadCSS(css){
+		if(window['x-style'] !== undefined){
+			window['x-style'] = window['x-style'] + css;
+		}else{
+			window['x-style'] = css;
+		}
+	}
+	function createScope(scope, component){
+		component.$scope.push(scope);
+	}
+	function applyClassName(props, component){
+		let classNames = props.classNames.split(" ");
+		classNames.forEach(function(name){
+			component.element.classList.add(name);
+		});
+	}
+	function applyRootStyle(props, component){
+		component.element.setAttribute("style", props.style);
+	}
+	function uniqueComponents(options){
+		let ucom = window['unique_components'];
+		if(ucom == undefined){
+			ucom = window['unique_components'] = new Set();
+		}
+		ucom.add(options.name);
+	}
+	
+	return function(props){
+		return init(props, options);
+	}
 }
-let x = new X();
 export default x;
